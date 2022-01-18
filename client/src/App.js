3ad97f2dc3cfbc3f35295ddce81c90e779aa408f
Lavanya Y C <78248268+Lavanya2099@users.noreply.github.com>
2022-01-18 10:22:24 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -31,7 +31,7 @@ export function App () {
             <Toolbar>
                 <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"><MenuIcon /></IconButton>
                 <Typography variant="h6" className={classes.title}>Resume Builder</Typography>
-              
+                 <Button color="inherit">Login</Button>
             </Toolbar>
         </AppBar>
         <Resume />
